70059e1abea0bd6f32662e0b6137f1c8afc76e10
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-07 21:32:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <? 
 /*
-    Copyright (C) 2013-2014 xtr4nge [_AT_] gmail.com
+    Copyright (C) 2013-2020 xtr4nge [_AT_] gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>FruityWifi</title>
+<title>FruityWifi : Meterpreter</title>
 <script src="../js/jquery.js"></script>
 <script src="../js/jquery-ui.js"></script>
 <link rel="stylesheet" href="../css/jquery-ui.css" />
@@ -51,7 +51,7 @@ include "../../functions.php";
 
 // Checking POST & GET variables...
 if ($regex == 1) {
-	regex_standard($_POST["newdata"], "msg.php", $regex_extra);
+    regex_standard($_POST["newdata"], "msg.php", $regex_extra);
     regex_standard($_GET["logfile"], "msg.php", $regex_extra);
     regex_standard($_GET["action"], "msg.php", $regex_extra);
     regex_standard($_POST["service"], "msg.php", $regex_extra);
@@ -63,38 +63,30 @@ $action = $_GET["action"];
 $tempname = $_GET["tempname"];
 $service = $_POST["service"];
 
-// DELETE LOG
-if ($logfile != "" and $action == "delete") {
-    $exec = "$bin_rm ".$mod_logs_history.$logfile.".log";
-    //exec("$bin_danger \"$exec\"", $dump); //DEPRECATED
-    exec_fruitywifi($exec);
-}
-
-// SET MODE
-if ($_POST["change_mode"] == "1") {
-    $ss_mode = $service;
-    $exec = "$bin_sed -i 's/ss_mode.*/ss_mode = \\\"".$ss_mode."\\\";/g' includes/options_config.php";
-    //exec("$bin_danger \"$exec\"", $output); //DEPRECATED
-    $output = exec_fruitywifi($exec);
-}
 
 ?>
 
 <div class="rounded-top" align="left"> &nbsp; <b><?=$mod_alias?></b> </div>
 <div class="rounded-bottom">
 
-    &nbsp;&nbsp;version <?=$mod_version?><br>
+    &nbsp;version <?=$mod_version?><br>
     
     <?
-    $ismoduleup = exec("ps auxww | grep $mod_name | grep -v -e 'grep $mod_name'");
-    if ($ismoduleup != "") {
-        echo "&nbsp;&nbsp;$mod_alias  <font color=\"lime\"><b>enabled</b></font>.&nbsp; | <a href=\"includes/module_action.php?service=$mod_name&action=stop&page=module\"><b>stop</b></a>";
-        echo "<input type='hidden' name='action' value='stop'>";
-        echo "<input type='hidden' name='page' value='module'>";
+    if (file_exists($payloc)){
+        echo "&nbsp; $mod_alias  <font color='lime'><b>build</b></font><br>";
     } else { 
-        echo "&nbsp;&nbsp;$mod_alias  <font color=\"red\"><b>disabled</b></font>. | <a href=\"includes/module_action.php?service=$mod_name&action=start&page=module\"><b>start</b></a>"; 
-        echo "<input type='hidden' name='action' value='start'>";
-        echo "<input type='hidden' name='page' value='module'>";
+        echo "&nbsp; $mod_alias  <font color='red'><b>needs build</b></font><br>"; 
+    }
+    ?>
+
+    <?
+    if (file_exists($payloc)){
+    $ismoduleup = exec($mod_isup);
+    if ($ismoduleup != "") {
+        echo "&nbsp;&nbsp;$mod_alias  <font color='lime'><b>enabled</b></font><br>";
+    } else { 
+        echo "&nbsp;&nbsp;$mod_alias  <font color='red'><b>disabled</b></font><br>"; 
+     }
     }
     ?>
 
@@ -112,11 +104,15 @@ Loading, please wait...
     <div id="result" class="module">
         <ul>
             <li><a href="#result-1">Setup</a></li>
-            <li><a href="#result-2">About</a></li>
+	        <li><a href="#result-2">Build</a></li>
+	        <li><a href="#result-3">Execute</a></li>
+            <li><a href="#result-4">About</a></li>
         </ul>
         <div id="result-1">
 	    <form method="POST" autocomplete="off" action="includes/save.php">
-                <div class="module-options">
+        <div class="module-options">
+		<h4>Setup</h4>
+		<p>Basic Payload Settings</p>
                     <table>
                         <tr>
                             <td>Host: </td>
@@ -136,12 +132,100 @@ Loading, please wait...
                     </table>
 		</div> 
 	    </form>
-            
         </div>
+
+	<!-- Build -->
+
+        <div id="result-2" class="history">
+        <div class="module-options">
+        <form method="POST" autocomplete="off" action="includes/module_action.php">
+		<h4>Build</h4>
+		<p>Msfvenom is the combination of payload generation and encoding. <br>
+		   It replaced msfpayload and msfencode on June 8th 2015.</p>
+		<p>Generate preconfigured Payloads Here</p>
+		<p>Generated Payload will be meterpreter_reverse_tcp (nonstager) !</p>
+            <select name="payloadtype" id="payloadtype" required>
+  			<option id="payloadtype" value="default" selected disabled>Select Payload Type</option>
+  			<option id="payloadtype" value="python">Python Payload (.py)</option>
+  			<option id="payloadtype" value="php">PHP Payload (.php)</option>
+			<option id="payloadtype" value="app">APP Payload (.apk)</option>
+  			<option id="payloadtype" value="exe">EXE Payload (.exe)</option>
+			<option id="payloadtype" value="osx">OSX EXE (.bin)</option>
+
+		    </select>
+		    <? 
+			$ismoduleup = exec($mod_building);
+            		if ($ismoduleup != "") {
+			echo "<button style='color:red' disabled>Build Running !</button>";
+			}else{
+			echo "<input name='action' type='hidden' value='build'>";
+		    echo "<button type='submit' style='color:red;'>Build</button>";
+			}
+		     ?> 
+            </form>
+	    <? 
+	    if (file_exists($payloc)){
+	    echo "<form method='POST' autocomplete='off' action='includes/module_action.php'>";
+	    echo "<br>";
+		$ismoduleup = exec($mod_isup);
+		if ($ismoduleup == "") {
+        		echo "<input name='action' type='hidden' value='deletepayload'>";
+        		echo "<button type='submit' style='color:red;'>Delete Payload</button>";   
+         	}
+	    echo "</form>";
+	    echo "<br>";
+	    echo "<a href='includes/builds/download.php'style='color:red;'><button>Download Payload</button></a>";	
+        }
+	    ?>             
+	   </div>
+        </div>
+  
+        <!-- END Build -->
+
+	<!-- EXECUTE -->
+
+        <div id="result-3" class="history">
+	<div class='module-options'>
+	<h4>Execute</h4>
+	<p sytle='color:red;'>Local Payload Execution !</p>	
+        <?
+	if (file_exists($payloc)){
+	echo "<p>Go On</p>";
+	}else{
+	echo "<p>Build First !</p>";
+	}
+	if (file_exists($payloc)){
+ 	    echo "<p>Execute Your Payload: <b> $payfile</b></p>";
+	    echo "<p>Type: $paytype | Comp: $paycomp</p>";
+	    echo "<form method='POST' autocomplete='off' action='includes/module_action.php'>";
+	    if ($paycomp == "true"){
+            echo "<input name='action' type='hidden' value='executepayload'>";
+            echo "<button type='submit' style='color:red;'>Execute Payload</button>";     
+      	    }else{
+            echo "<button  style='color:red;' disabled>Payload not compatible !</button>"; 
+	    }
+	    echo "</form>";
+	    
+	    $ismoduleup = exec($mod_isup);
+	    if ($ismoduleup != "") {
+	    echo "<form method='POST' autocomplete='off' action='includes/module_action.php'>";
+	    echo "<br>";
+            echo "<input name='action' type='hidden' value='stoppayload'>";
+            echo "<button type='submit' style='color:red;'>Stop Payload</button>";
+            echo "</form>";
+	    }
+        }
+	?>  
+
+
+          </div>
+        </div>
+
+        <!-- END EXECUTE -->
 
         <!-- ABOUT -->
 
-        <div id="result-2" class="history">
+        <div id="result-4" class="history">
             <? include "includes/about.php"; ?>
         </div>
 
@@ -261,7 +345,17 @@ Loading, please wait...
         echo "<script>";
         echo "$( '#result' ).tabs({ active: 4 });";
         echo "</script>";
+    } else if ($_GET["tab"] == 5) {
+        echo "<script>";
+        echo "$( '#result' ).tabs({ active: 5 });";
+        echo "</script>";
+    } else if ($_GET["tab"] == 6) {
+        echo "<script>";
+        echo "$( '#result' ).tabs({ active: 6 });";
+        echo "</script>";
     } 
+
+
     ?>
 
 </div>
@@ -272,6 +366,5 @@ $(document).ready(function() {
     $('#msg').hide();
 });
 </script>
-
 </body>
 </html>
