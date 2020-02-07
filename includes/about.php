a5f07b760909c08b99ed7d26e77424a7b354f35a
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-07 21:34:16 +0100

    Update about.php

--- includes/about.php
+++ includes/about.php
@@ -1,1 +1,9 @@
-<b>Meterpreter</b> is an advanced, dynamically extensible payload that uses in-memory DLL injection stagers and is extended over the network at runtime. It communicates over the stager socket and provides a comprehensive client-side Ruby API. It features command history, tab completion, channels, and more. Metepreter was originally written by skape for Metasploit 2.x, common extensions were merged for 3.x and is currently undergoing an overhaul for Metasploit 3.3. The server portion is implemented in plain C and is now compiled with MSVC, making it somewhat portable. The client can be written in any language but Metasploit has a full-featured Ruby client API.
+<b>Meterpreter</b> is an advanced, dynamically extensible payload that uses in-memory DLL injection stagers and is extended over the network at runtime. <br>
+It communicates over the stager socket and provides a comprehensive client-side Ruby API. It features command history, tab completion, channels, and more. <br>
+Metepreter was originally written by skape for Metasploit 2.x, common extensions were merged for 3.x and is currently undergoing an overhaul for Metasploit 3.3. <br>
+The server portion is implemented in plain C and is now compiled with MSVC, making it somewhat portable. <br>
+The client can be written in any language but Metasploit has a full-featured Ruby client API. <br>
+<br><br>
+This Module allows you to build common payloads. <br>
+Execute allows you to build a custom python payload and connect your fruity with an existing listener <br>
+
