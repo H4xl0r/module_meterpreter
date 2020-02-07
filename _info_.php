aa8600257f7c027deab390996e0cd90e85ee0038
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-07 21:32:26 +0100

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,27 +1,34 @@
 <?
 $mod_name="meterpreter";
-$mod_version="1.2";
+$mod_version="2.b";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
-$mod_logs_panel="disabled";
+$mod_logs_panel="enabled";
 $mod_panel="show";
-$meterpreter_host = "10.0.0.1";
-$meterpreter_port = "6666";
-$mod_isup="netstat -anp|grep -iEe '".$meterpreter_port.".+ESTABLISHED'";
+$mod_type="service";
+$mod_isup="ps auxww | grep -iEe 'payload' | grep -v -e grep";
+$mod_building="ps auxww | grep -iEe 'msfvenom' | grep -v -e grep";
 $mod_alias="Meterpreter";
 
+# USER OPT
+$meterpreter_host = "10.0.0.1";
+$meterpreter_port = "666";
+$payfile = "payload.bin";
+$payloc = "$mod_path/includes/builds/$payfile";
+$paytype = "bin";
+$paycomp = "false";
+
+
 # EXEC
-$bin_danger = "/usr/share/fruitywifi/bin/danger";
 $bin_sudo = "/usr/bin/sudo";
+$bin_msfvenom = "/usr/bin/msfvenom";
 $bin_sh = "/bin/sh";
 $bin_echo = "/bin_echo";
-$bin_tcpdump = "/usr/sbin/tcpdump";
 $bin_killall = "/usr/bin/killall";
 $bin_cp = "/bin/cp";
 $bin_chmod = "/bin/chmod";
 $bin_sed = "/bin/sed";
 $bin_rm = "/bin/rm";
-$bin_php = "/usr/bin/php";
 $bin_kill = "/bin/kill";
 ?>
